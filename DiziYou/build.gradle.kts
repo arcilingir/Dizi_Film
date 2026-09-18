8906785f188d4c45a6985fe5b2094bf91f8a0e80
Ali Rıza Cilingir <49230729+arcilingir@users.noreply.github.com>
2026-09-18 16:35:02 +0300

    Update build.gradle.kts

--- DiziYou/build.gradle.kts
+++ DiziYou/build.gradle.kts
@@ -1,7 +1,7 @@
 version = 18
 
 cloudstream {
-    authors     = listOf("keyiflerolsun", "nikyokki")
+    authors     = listOf("keyiflerolsun", "arcilingir")
     language    = "tr"
     description = "Diziyou en kaliteli Türkçe dublaj ve altyazılı yabancı dizi izleme sitesidir. Güncel ve efsanevi dizileri 1080p Full HD kalitede izlemek için hemen tıkla!"
 
