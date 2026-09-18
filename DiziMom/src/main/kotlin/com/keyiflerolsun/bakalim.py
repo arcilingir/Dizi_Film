d5cb7e34c3d354bed407618f259d547a11fdc12f
Ali Rıza Cilingir <49230729+arcilingir@users.noreply.github.com>
2026-09-18 17:49:36 +0300

    Update URLs for login and content retrieval

--- DiziMom/src/main/kotlin/com/keyiflerolsun/bakalim.py
+++ DiziMom/src/main/kotlin/com/keyiflerolsun/bakalim.py
@@ -12,23 +12,23 @@ oturum.headers.update({"User-Agent": "Mozilla/5.0 (Linux; Android 10; K) AppleWe
 # oturum.cookies.update({"wordpress_logged_in_7e0a80686bffd7035218d41e8240d65f": "keyiflerolsun|1704461004|TJh8nQRYrqZ9xlAyO7rO5QgiqTQiw7op8I6LKkSvytX|0dc654883fad5f0301df32e6465aa676a3f235288dd9c8a73260d9c8a20b19ae"})
 
 oturum.post(
-    url  = "https://www.dizimom.im/wp-login.php",
+    url  = "https://www.dizimom.diy/wp-login.php",
     data = {
         "log"         : "keyiflerolsun",
         "pwd"         : "12345",
         "rememberme"  : "forever",
-        "redirect_to" : "https://www.dizimom.im",
+        "redirect_to" : "https://www.dizimom.diy",
     }
 )
 
-istek  = oturum.get("https://www.dizimom.im/modern-dogu-masallari-1-sezon-7-bolum-izle/")
+istek  = oturum.get("https://www.dizimom.diy/modern-dogu-masallari-1-sezon-7-bolum-izle/")
 konsol.print(istek.url)
 
 secici = Selector(istek.text)
 iframe = secici.css("div.video p iframe::attr(src)").get()
 konsol.print(iframe)
 
-oturum.headers.update({"Referer": "https://www.dizimom.im/"})
+oturum.headers.update({"Referer": "https://www.dizimom.diy/"})
 i_source = oturum.get(iframe).text
 
 be_player = search(r"bePlayer\('([^']+)',\s*'(\{[^\}]+\})'\);", i_source).groups()
