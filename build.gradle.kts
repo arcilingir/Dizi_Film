694f2a1e9ea511ffcd54b75f347c286bf7185fd7
Ali Rıza Cilingir <49230729+arcilingir@users.noreply.github.com>
2026-09-25 22:31:04 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ buildscript {
     dependencies {
         classpath("com.android.tools.build:gradle:8.2.2")
         
-        // Doğru grup adresi ve master bağımlılığı:
+        // DİKKAT: 'recloudstream.gradle' DEĞİL, iki nokta ile 'recloudstream:gradle' olmalı:
         classpath("com.github.recloudstream:gradle:master-SNAPSHOT")
         
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
