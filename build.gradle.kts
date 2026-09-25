606823d238f0c2a2ffdde35a9c9d9685ba6a00ce
Ali Rıza Cilingir <49230729+arcilingir@users.noreply.github.com>
2026-09-25 22:37:50 +0300

    Update Recloudstream gradle dependency to master-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ buildscript {
     }
     dependencies {
         classpath("com.android.tools.build:gradle:8.2.2")
-        classpath("com.github.recloudstream:gradle:pre-RELEASE")
+        classpath("com.github.recloudstream:gradle:master-SNAPSHOT")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
     }
 }
